a8b5513487a99457f4ec7149ae133fecda7e3128
CoderShr <103101677+CoderShr@users.noreply.github.com>
2022-05-19 17:24:00 +0000

    Add files via upload

---
+++ food-service-config-server/src/main/java/com/order/configServer/foodserviceconfigserver/FoodServiceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.order.configServer.foodserviceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServiceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodServiceConfigServerApplication.class, args);
+	}
+
+}
---
+++ food-service-config-server/src/test/java/com/order/configServer/foodserviceconfigserver/FoodServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.configServer.foodserviceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
